81189a554e8e4bf1e782b7fe16923da9a1336852
Kevin Nguyen <kevin.nguyen7100@gmail.com>
2024-03-15 22:07:22 -0700

    Resolving merge conflict

--- src/components/Main.js
+++ src/components/Main.js
@@ -36,7 +36,7 @@ function Main() {
       <br></br>
 
       <div className='section3'>
-      <img src={dog} className='mainPicture'></img>
+      <img src={slenderman} className='mainPicture'></img>
         <div className='text'>
           <h2 id="Experience"> Experience </h2>
           <p> Some programming languages I know are: </p>
